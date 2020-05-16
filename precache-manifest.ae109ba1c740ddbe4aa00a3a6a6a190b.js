0b91012bd23afc1d90f71f740c79ef587c9b6402
Aneta Jagla <anett@poczta.fm>
2020-05-16 12:12:20 +0200

    Updates

---
+++ precache-manifest.ae109ba1c740ddbe4aa00a3a6a6a190b.js
@@ -0,0 +1,270 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d20167e650facd03f5a9450bb902820b",
+    "url": "/polski/index.html"
+  },
+  {
+    "revision": "09a79ddebef8737e16ff",
+    "url": "/polski/static/css/2.3761eb0c.chunk.css"
+  },
+  {
+    "revision": "09a79ddebef8737e16ff",
+    "url": "/polski/static/js/2.d757da1a.chunk.js"
+  },
+  {
+    "revision": "cc9c1048a65a0217e7c68d61ece799eb",
+    "url": "/polski/static/js/2.d757da1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f882d656896467263b1c",
+    "url": "/polski/static/js/main.4bafdacc.chunk.js"
+  },
+  {
+    "revision": "9872a23608ba191ca251",
+    "url": "/polski/static/js/runtime-main.a1889fef.js"
+  },
+  {
+    "revision": "3fa7655d572b0f1b72e32bce78c2118f",
+    "url": "/polski/static/media/bajka.3fa7655d.mp3"
+  },
+  {
+    "revision": "2a8fd91ba78d0fc447666d2b11507ccb",
+    "url": "/polski/static/media/bialy.2a8fd91b.mp3"
+  },
+  {
+    "revision": "2d4d8971e0b14962afcc5107c98e774d",
+    "url": "/polski/static/media/brudny.2d4d8971.mp3"
+  },
+  {
+    "revision": "8cab0fa836b7c68200f5458dcded57dc",
+    "url": "/polski/static/media/brzydki.8cab0fa8.mp3"
+  },
+  {
+    "revision": "81f813a072b5df5248d8f9c500e710e3",
+    "url": "/polski/static/media/cichy.81f813a0.mp3"
+  },
+  {
+    "revision": "d0c415afdb578e74ce16ba7bfe73ecb2",
+    "url": "/polski/static/media/cieplo.d0c415af.mp3"
+  },
+  {
+    "revision": "7f0d107aa3fb824117530fbcfab35fbe",
+    "url": "/polski/static/media/czarny.7f0d107a.mp3"
+  },
+  {
+    "revision": "b199a6d2531e5f20f15ca31db75482c1",
+    "url": "/polski/static/media/czysty.b199a6d2.mp3"
+  },
+  {
+    "revision": "ed7be3aee65ce1ef3c15e234ecc8056e",
+    "url": "/polski/static/media/czytac.ed7be3ae.mp3"
+  },
+  {
+    "revision": "5d7a18fc01838c2068141e32edab07ba",
+    "url": "/polski/static/media/dlugopis.5d7a18fc.mp3"
+  },
+  {
+    "revision": "2ab4997c37c9f08af5ae6e6a5bae0caa",
+    "url": "/polski/static/media/dobry.2ab4997c.mp3"
+  },
+  {
+    "revision": "16ffceaa1d8069be3b56d1693da853e5",
+    "url": "/polski/static/media/dom.16ffceaa.mp3"
+  },
+  {
+    "revision": "0d0d2f8b2304224b7bb9b7198ee4d95f",
+    "url": "/polski/static/media/duzy.0d0d2f8b.mp3"
+  },
+  {
+    "revision": "1865ca0bed164c61872a01240eb175a2",
+    "url": "/polski/static/media/dzien.1865ca0b.mp3"
+  },
+  {
+    "revision": "0464e5aa7d7bbd7b2ae1a4ca56fe4f03",
+    "url": "/polski/static/media/film.0464e5aa.mp3"
+  },
+  {
+    "revision": "ef706ba992c07581cf1964777e6f179b",
+    "url": "/polski/static/media/glodny.ef706ba9.mp3"
+  },
+  {
+    "revision": "bafa4f9e78b67c8c35062fc909cd4a2a",
+    "url": "/polski/static/media/gory.bafa4f9e.mp3"
+  },
+  {
+    "revision": "479dcbfabad71f4f56a4211fd04da52b",
+    "url": "/polski/static/media/ja.479dcbfa.mp3"
+  },
+  {
+    "revision": "9e460683e12b14bbb3028bbd5098ae2e",
+    "url": "/polski/static/media/jesc.9e460683.mp3"
+  },
+  {
+    "revision": "f789bef9dc330dbf6d49ea14c2e6dfa7",
+    "url": "/polski/static/media/jezdzic.f789bef9.mp3"
+  },
+  {
+    "revision": "314376a51d2b347aae7a445c11da9944",
+    "url": "/polski/static/media/kanapka.314376a5.mp3"
+  },
+  {
+    "revision": "687e335c6c8c79751bdaadbe33549a5f",
+    "url": "/polski/static/media/kierowcaTramwaju.687e335c.mp3"
+  },
+  {
+    "revision": "f8d03fb5598787905e322f7078f60df1",
+    "url": "/polski/static/media/kochac.f8d03fb5.mp3"
+  },
+  {
+    "revision": "824bbebef82004f862ad090b95978158",
+    "url": "/polski/static/media/kolacja.824bbebe.mp3"
+  },
+  {
+    "revision": "00b67039b07a90891da8066398b30d83",
+    "url": "/polski/static/media/ksiazka.00b67039.mp3"
+  },
+  {
+    "revision": "6eeb1968505ebd066abc058b3a53fe5a",
+    "url": "/polski/static/media/ladny.6eeb1968.mp3"
+  },
+  {
+    "revision": "153d21944c5f09030a337b9e27581766",
+    "url": "/polski/static/media/lubic.153d2194.mp3"
+  },
+  {
+    "revision": "ac974633bc6cbd2778bb89e9ffa4716d",
+    "url": "/polski/static/media/maly.ac974633.mp3"
+  },
+  {
+    "revision": "d36f077fa06108f6e354ceb95cc6e44a",
+    "url": "/polski/static/media/marchewka.d36f077f.mp3"
+  },
+  {
+    "revision": "b3fb80980bd0470eef30390fe8389ab8",
+    "url": "/polski/static/media/morze.b3fb8098.mp3"
+  },
+  {
+    "revision": "ebbe4f580c3f9b51d3536a2c9d6cdd79",
+    "url": "/polski/static/media/mowic.ebbe4f58.mp3"
+  },
+  {
+    "revision": "1b84eb1a2f8dee3343bb3b89b3fd2926",
+    "url": "/polski/static/media/my.1b84eb1a.mp3"
+  },
+  {
+    "revision": "17a4bb06e0f8ee4156b8ea8d4aac9da8",
+    "url": "/polski/static/media/noc.17a4bb06.mp3"
+  },
+  {
+    "revision": "13e8060173c8b6e0f309853dc06c8382",
+    "url": "/polski/static/media/nowy.13e80601.mp3"
+  },
+  {
+    "revision": "f91c0e3ed3f1dd24cd423b43327682e1",
+    "url": "/polski/static/media/obiad.f91c0e3e.mp3"
+  },
+  {
+    "revision": "8c3830891c7f53d2f17910879b084c6d",
+    "url": "/polski/static/media/ogladac.8c383089.mp3"
+  },
+  {
+    "revision": "acebdff15cea56069ccb0d416d78e673",
+    "url": "/polski/static/media/okno.acebdff1.mp3"
+  },
+  {
+    "revision": "1f892d3e3bb2f349685b6048f354f009",
+    "url": "/polski/static/media/on.1f892d3e.mp3"
+  },
+  {
+    "revision": "4f19b111ed29a3ba23774e9ee6fad540",
+    "url": "/polski/static/media/ona.4f19b111.mp3"
+  },
+  {
+    "revision": "9d07146dcb6deb7a2b3d8daabeb50f5c",
+    "url": "/polski/static/media/one.9d07146d.mp3"
+  },
+  {
+    "revision": "81b628ef052b93ee588c172232afc255",
+    "url": "/polski/static/media/oni.81b628ef.mp3"
+  },
+  {
+    "revision": "493a5597e31de0511cfb24bb271b7253",
+    "url": "/polski/static/media/ono.493a5597.mp3"
+  },
+  {
+    "revision": "d7c32eccbe83c98fa8774b5de1d5fc73",
+    "url": "/polski/static/media/pic.d7c32ecc.mp3"
+  },
+  {
+    "revision": "dfa1668dbc9399f2d0638fd2499d8156",
+    "url": "/polski/static/media/pisac.dfa1668d.mp3"
+  },
+  {
+    "revision": "fa40aa6a099522bfaf69cd676ba26617",
+    "url": "/polski/static/media/praca.fa40aa6a.mp3"
+  },
+  {
+    "revision": "01afe228245fd787486f0bd7b5749bf9",
+    "url": "/polski/static/media/pracowac.01afe228.mp3"
+  },
+  {
+    "revision": "984a8095cc40494cd900e4ab4df96630",
+    "url": "/polski/static/media/punktualny.984a8095.mp3"
+  },
+  {
+    "revision": "f673979a381bc6111d23fe12dbd6b583",
+    "url": "/polski/static/media/sklep.f673979a.mp3"
+  },
+  {
+    "revision": "ee966c514e4de9620ed6ec78fd686be0",
+    "url": "/polski/static/media/sluchac.ee966c51.mp3"
+  },
+  {
+    "revision": "4cc97940e5509c573d8bfef45aa372bb",
+    "url": "/polski/static/media/smutny.4cc97940.mp3"
+  },
+  {
+    "revision": "bd2f97ee33283a4c9d95075f8a345602",
+    "url": "/polski/static/media/spac.bd2f97ee.mp3"
+  },
+  {
+    "revision": "3b45ac5ceaea8cd2b8108cb199c75a32",
+    "url": "/polski/static/media/stary.3b45ac5c.mp3"
+  },
+  {
+    "revision": "820de2fe313a7d26d9e041a0a1842af1",
+    "url": "/polski/static/media/szanowac.820de2fe.mp3"
+  },
+  {
+    "revision": "4d1dabe69fb80fffb46d7d3ed4c0ddf9",
+    "url": "/polski/static/media/telewizor.4d1dabe6.mp3"
+  },
+  {
+    "revision": "4c5b8aad7ee942f451ee2a5d4099d667",
+    "url": "/polski/static/media/tramwaj.4c5b8aad.mp3"
+  },
+  {
+    "revision": "56362023f8db0a038ca659e3d6d268af",
+    "url": "/polski/static/media/ty.56362023.mp3"
+  },
+  {
+    "revision": "9f5febe8ac6b4a15fb4f13f2a292d677",
+    "url": "/polski/static/media/wesoly.9f5febe8.mp3"
+  },
+  {
+    "revision": "02c42d4e75971718417da201730cffaa",
+    "url": "/polski/static/media/wy.02c42d4e.mp3"
+  },
+  {
+    "revision": "dd6e3a33ea7641797c2cf13043930301",
+    "url": "/polski/static/media/zabawa.dd6e3a33.mp3"
+  },
+  {
+    "revision": "cc899193834cc6de2945c8ea70f1ee12",
+    "url": "/polski/static/media/zimno.cc899193.mp3"
+  },
+  {
+    "revision": "2b5d4a0a8f879ce7c4f760f0899c43f0",
+    "url": "/polski/static/media/zly.2b5d4a0a.mp3"
+  }
+]);
